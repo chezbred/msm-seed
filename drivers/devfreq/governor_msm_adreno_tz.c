78073948dbda6f957e378a63e31a98c3314613ed
PineappleOwl <aryneneilcabatan@gmail.com>
2016-02-12 18:13:24 +0800

    FIX DERP SIMPLE GPU ALGO- HEADER

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -223,11 +223,6 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		scm_data[0] = level;
 		scm_data[1] = priv->bin.total_time;
 		scm_data[2] = priv->bin.busy_time;
-<<<<<<< HEAD
-		__secure_tz_update_entry3(scm_data, sizeof(scm_data),
-					&val, sizeof(val), priv->is_64);
-=======
->>>>>>> 66673b1... fix derps
 #ifdef CONFIG_SIMPLE_GPU_ALGORITHM
 		if (simple_gpu_active != 0)
 			val = simple_gpu_algorithm(level, priv);
